1df8fd6c1f3303a78ac911ed01b0b58a3a0af556
Andrew Jackson <andrew@codechimp.org>
2024-08-02 11:12:45 +0100

    Fixes

--- .github/scripts/library_doc/generate_file.py
+++ .github/scripts/library_doc/generate_file.py
@@ -43,11 +43,11 @@ def generate_device_list():
         model = device["model"]
         model_match_method = device.get("model_match_method", "")
         if model_match_method == "startswith":
-            model = f"*{model}"
+            model = f"\*{model}"
         if model_match_method == "endswith":
-            model = f"{model}*"
+            model = f"{model}\*"
         if model_match_method == "contains":
-            model = f"*{model}*"
+            model = f"\*{model}\*"
 
         row = [
             device["manufacturer"],
