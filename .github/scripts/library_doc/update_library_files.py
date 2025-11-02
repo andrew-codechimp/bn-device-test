8a04d29b0ac8701a31270a53e51312601e07b77b
Andrew Jackson <andrew@codechimp.org>
2025-11-02 15:12:26 +0000

    Sync with BN

--- .github/scripts/library_doc/update_library_files.py
+++ .github/scripts/library_doc/update_library_files.py
@@ -1,37 +1,43 @@
 """Battery library document generator."""
 
-from __future__ import annotations
-
 import json
 
 from pytablewriter import MarkdownTableWriter
 
 
-def generate_device_list():
-    """Generate static file containing the device library."""
-
+def get_device_list() -> None:
+    """Sort the device library JSON file."""
     # Load the existing JSON library file
-    with open("library/library.json", encoding="UTF-8") as f:
+    with open(
+        file="library/library.json",
+        encoding="UTF-8",
+    ) as f:
         devices_json = json.loads(f.read())
         devices = devices_json.get("devices")
 
-    toc_links: list[str] = []
-    tables_output: str = ""
+    # Sort the devices by manufacturer and model
+    devices.sort(
+        key=lambda k: (
+            k["manufacturer"].lower(),
+            k.get("model_match_method", "").lower(),
+            k["model"].lower(),
+            k.get("model_id", "").lower(),
+            k.get("hw_version", "").lower(),
+        )
+    )
+    with open("library/library.json", "w", encoding="UTF-8") as f:
+        f.write(json.dumps(devices_json, indent=4))
+
+
+def regenerate_device_list() -> None:
+    """Generate static file containing the device library."""
+
+    # Load the existing JSON library file
+    with open("library/library.json", encoding="UTF-8") as file:
+        devices_json = json.loads(file.read())
+        devices = devices_json.get("devices")
+
     rows = []
-
-    num_devices = len(devices)
-
-    writer = MarkdownTableWriter()
-    headers = [
-        "Manufacturer",
-        "Model",
-        "Battery Type",
-        "Model ID (optional)",
-        "HW Version (optional)",
-    ]
-
-    writer.header_list = headers
-
     for device in devices:
         if device.get("battery_quantity", 1) > 1:
             battery_type_qty = f"{device['battery_quantity']}× {device['battery_type']}"
@@ -56,15 +62,28 @@ def generate_device_list():
         ]
         rows.append(row)
 
+    writer = MarkdownTableWriter()
+    writer.header_list = [
+        "Manufacturer",
+        "Model",
+        "Battery Type",
+        "Model ID (optional)",
+        "HW Version (optional)",
+    ]
     writer.value_matrix = rows
-    tables_output += f"## {num_devices} Devices in library\n\n"
-    tables_output += "This file is auto generated, do not modify\n\n"
-    tables_output += "Request new devices to be added to the library [here](https://github.com/andrew-codechimp/HA-Battery-Notes/issues/new?template=new_device_request.yml&title=%5BDevice%5D%3A+)\n\n"
-    tables_output += writer.dumps()
+
+    tables_output = [f"## {len(devices)} Devices in library\n\n"]
+    tables_output.append("This file is auto generated, do not modify\n\n")
+    tables_output.append(
+        "Request new devices to be added to the library [here](https://github.com/andrew-codechimp/HA-Battery-Notes/issues/new?template=new_device_request.yml&title=%5BDevice%5D%3A+)\n\n"
+    )
+    tables_output.append(writer.dumps())
 
     with open("library.md", "w", encoding="UTF-8") as md_file:
-        md_file.write("".join(toc_links) + tables_output)
+        md_file.write("".join(tables_output))
         md_file.close()
 
 
-generate_device_list()
+if __name__ == "__main__":
+    get_device_list()
+    regenerate_device_list()
